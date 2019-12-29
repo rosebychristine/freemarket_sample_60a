030bc07ea845e2da597e26cafaab990c5a1245c5
近藤正道 <kuros96@icloud.comgit config --global user.name masamichi0963>
2019-12-29 15:50:00 +0900

    edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  resources :signup, only: [:index, :create] do
-    collection do
-      get 'registration'
-    end
+  root 'signups#index'  
+  get  'signups/index'  =>  'signups#index'
   end
-end
