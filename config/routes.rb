328421744f18c741561c3cc70a5ff5e9a537bfc9
近藤正道 <kuros96@icloud.comgit config --global user.name masamichi0963>
2020-01-18 15:12:10 +0900

    edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root 'signups#index'  
   get  'signups/index'  =>  'signups#index'
+  get  'signups/edit' => 'signups#edit'
+  get  'signups/edit2' => 'signups#edit2'
+  get  'signups/edit3' => 'signups#edit3'
+  get  'signups/edit4' => 'signups#edit4'
+  get  'signups/edit5' => 'signups#edit5'
   end
