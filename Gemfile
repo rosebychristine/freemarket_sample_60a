f2301984d84174d2b2763ee4f40939a2f0b8d2d6
近藤正道 <kuros96@icloud.comgit config --global user.name masamichi0963>
2020-01-19 12:04:46 +0900

    conflict

--- Gemfile
+++ Gemfile
@@ -71,5 +71,3 @@ gem 'font-awesome-rails'
 group :production do
   gem 'unicorn', '5.4.1'
 end
-
-gem 'haml-rails'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,20 @@
 Rails.application.routes.draw do
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
   resources :signups, only: [:index, :create] do
     collection do
       get 'member_info'
