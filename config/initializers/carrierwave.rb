3a1a9267b8ee64d8aafbb17c4429e8ba370c7117
rosebychristine <43508812+rosebychristine@users.noreply.github.com>
2020-01-29 18:02:55 +0900

    update credential.yaml

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,11 +7,12 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: Rails.application.secrets.aws_access_key_id,
-    aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
+    aws_access_key_id: Rails.application.credentials.aws[:aws_access_key_id],
+    aws_secret_access_key: Rails.application.secrets.aws[:aws_secret_access_key],
     region: 'ap-northeast-1'
   }
 
   config.fog_directory  = 'expertfreesample60aaa'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/expertfreesample60aaa'
 end
+
