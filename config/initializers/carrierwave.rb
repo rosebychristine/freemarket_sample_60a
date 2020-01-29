347c000d3067d52077f7c542f3ae3fd0030c5cbf
rosebychristine <43508812+rosebychristine@users.noreply.github.com>
2020-01-29 20:30:37 +0900

    carrierwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,12 +7,11 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: Rails.application.credentials.aws[:aws_access_key_id],
-    aws_secret_access_key: Rails.application.secrets.aws[:aws_secret_access_key],
+    aws_access_key_id: Rails.application.fog_credentials.aws[:aws_access_key_id],
+    aws_secret_access_key: Rails.application.secrets.aws[:secret_access_key],
     region: 'ap-northeast-1'
   }
 
   config.fog_directory  = 'expertfreesample60aaa'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/expertfreesample60aaa'
 end
-
