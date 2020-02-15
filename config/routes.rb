eaa8f06d639a86052c9c0b7ca4e936cc34045b55
近藤正道 <kuros96@icloud.comgit config --global user.name masamichi0963>
2020-02-15 20:08:35 +0900

    routesの理想をコメントアウトで送信

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
-Rails.application.routes.draw do  
+Rails.application.routes.draw do
+#   resources :users, only: [:index, :create, :edit, :update, :show]
+#   resources :items, only: [:index, :create, :edit, :update, :show]
+# end
+
   resources :signups, only: [:index, :create] do
     collection do
       get 'member_info',to: 'signups#member_info'
