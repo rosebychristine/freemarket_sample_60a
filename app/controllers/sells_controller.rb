75f6fc888fdbad0b0751f03e3d6c26f27a215de6
pilons <56548247+pilons@users.noreply.github.com>
2020-02-15 15:50:38 +0900

    いったんマージ

--- app/controllers/sells_controller.rb
+++ app/controllers/sells_controller.rb
@@ -3,5 +3,10 @@ class SellsController < ApplicationController
     end
 
     def new
+       Product.create(sell_params)
+    end
+
+    def sell_params
+        params.permit(:name, :price).merge(user_id: current_user.id)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_08_070140) do
+ActiveRecord::Schema.define(version: 2020_02_11_120638) do
+
+  create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", default: "", null: false
+    t.integer "price", default: 0, null: false
+    t.string "condition", default: "", null: false
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_products_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -24,4 +39,5 @@ ActiveRecord::Schema.define(version: 2020_02_08_070140) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "products", "users"
 end
