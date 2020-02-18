9f72316ac5d03750771e985e42aba9c6a070f519
rosebychristine <43508812+rosebychristine@users.noreply.github.com>
2020-02-18 20:57:47 +0900

    items#indexの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 
 Rails.application.routes.draw do
   devise_for :users
+  root 'items#index'
   devise_scope :user do 
     get "sign_in", :to => "users/sessions#new"
     get "sign_out", :to => "users/sessions#destroy" 
@@ -13,6 +14,7 @@ Rails.application.routes.draw do
       get 'step4'
     end
   end
+
   resources :users, only: [:index,:edit]
   resources :items, only: [:new,:show,:update]
   resources :mypages, only: [:index,:profile,:show] 
