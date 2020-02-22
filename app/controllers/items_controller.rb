0b712a0124bfbf3842417fc44d8dc5ba71be862c
rosebychristine <43508812+rosebychristine@users.noreply.github.com>
2020-02-22 19:32:05 +0900

    image modelの編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,20 +1,19 @@
 class ItemsController < ApplicationController
 
     def index
-        @items = Item.includes(:images).order('created_at DESC')
-      @products = Product.order("created_at DESC").limit(5)
+      @product = Product.order("created_at DESC").limit(5)
     end
 
     def new
     # 出品画面を表示するためのアクション
     # form_withを使うためにまずインスタンス生成をする（空っぽのインスタンスの準備）
     @product = Product.new
-
     end
 
     def create
     # 保存する処理
         @product = Product.create(product_params)
+        @product = Item.includes(:images).order('created_at DESC').limit(10)
 
     end
 
@@ -25,13 +24,10 @@ class ItemsController < ApplicationController
     end
 
     def show
+        @product = Product.find(params[:id])
     end
 
-    def new
-        @product = Product.new
-        @Product = Product.find(params[:id])
-    end 
     def product_params
-        params.require(:product).permit(:name, :price ,:description).merge(user_id: 1)
+        params.require(:product).permit(:name, :price ,:description, images_attributes: {image: []}).merge(user_id: 1)
     end
 end
