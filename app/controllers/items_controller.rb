a189f3f8f03c89445f33e7f345852a280f1caf6b
arata6699 <arata.66@icloud.com>
2020-02-23 12:15:35 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,6 +10,8 @@ class ItemsController < ApplicationController
     # 出品画面を表示するためのアクション
     # form_withを使うためにまずインスタンス生成をする（空っぽのインスタンスの準備）
     @product = Product.new
+    @product.images.new
+     product_condition
     fee_budd 
     sipping_time
     prefectures 
@@ -35,27 +37,6 @@ class ItemsController < ApplicationController
     end
 
 
-    def fee_budd 
-      @fee_budd = ["送料込み(出品者負担)", "着払い(購入者負担)"]
-    end
-
-    def sipping_time
-      @sipping_time = ["1~2日で発送", "2~3日で発送", "4~7日で発送"]
-    end
-
-    def prefectures
-      @prefectures = ["北海道","青森県","岩手県","宮城県","秋田県","山形県","福島県",
-            "茨城県","栃木県","群馬県","埼玉県","千葉県","東京都","神奈川県",
-            "新潟県","富山県","石川県","福井県","山梨県","長野県",
-            "岐阜県","静岡県","愛知県","三重県",
-            "滋賀県","京都府","大阪府","兵庫県","奈良県","和歌山県",
-            "鳥取県","島根県","岡山県","広島県","山口県",
-            "徳島県","香川県","愛媛県","高知県",
-            "福岡県","佐賀県","長崎県","熊本県","大分県","宮崎県","鹿児島県", 
-            "沖縄県"]
-    end
-
-
     private
 
     def product_params
@@ -64,4 +45,24 @@ class ItemsController < ApplicationController
     def product_condition
         @condition = ["新品、未使用","目立った傷や汚れなし","やや傷や汚れあり","傷や汚れあり","全体的に状態が悪い"]
     end
+    def fee_budd 
+        @fee_budd = ["送料込み(出品者負担)", "着払い(購入者負担)"]
+      end
+  
+      def sipping_time
+        @sipping_time = ["1~2日で発送", "2~3日で発送", "4~7日で発送"]
+      end
+  
+      def prefectures
+        @prefectures = ["北海道","青森県","岩手県","宮城県","秋田県","山形県","福島県",
+              "茨城県","栃木県","群馬県","埼玉県","千葉県","東京都","神奈川県",
+              "新潟県","富山県","石川県","福井県","山梨県","長野県",
+              "岐阜県","静岡県","愛知県","三重県",
+              "滋賀県","京都府","大阪府","兵庫県","奈良県","和歌山県",
+              "鳥取県","島根県","岡山県","広島県","山口県",
+              "徳島県","香川県","愛媛県","高知県",
+              "福岡県","佐賀県","長崎県","熊本県","大分県","宮崎県","鹿児島県", 
+              "沖縄県"]
+      end
+  
 end
