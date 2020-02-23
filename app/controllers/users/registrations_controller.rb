3262cd0cb7057eb0f19741b34f7352c0f01f2ad4
rosebychristine <43508812+rosebychristine@users.noreply.github.com>
2020-02-23 13:26:36 +0900

    change

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,38 +1,38 @@
-# frozen_string_literal: true
+# # frozen_string_literal: true
 
-class Users::RegistrationsController < Devise::RegistrationsController
-  before_action :configure_sign_up_params, only: [:create]
-  before_action :configure_account_update_params, only: [:update]
+# class Users::RegistrationsController < Devise::RegistrationsController
+#   before_action :configure_sign_up_params, only: [:create]
+#   before_action :configure_account_update_params, only: [:update]
 
-  def new
-    super
-  end
+#   def new
+#     super
+#   end
 
-  def create
-    super
-  end
+#   def create
+#     super
+#   end
 
-  def member_info
+#   def member_info
 
-  end
+#   end
 
-  def phone_number
-    current_user.assign_attributes(account_update_params)
-    if current_user.save
-      redirect_to jp_signup_address_path
-    else
-      render "member_info"
-    end
-  end
+#   def phone_number
+#     current_user.assign_attributes(account_update_params)
+#     if current_user.save
+#       redirect_to jp_signup_address_path
+#     else
+#       render "member_info"
+#     end
+#   end
 
-  def phone_number_conf
+#   def phone_number_conf
 
-  end
+#   end
 
-  def postal_code
-  end
+#   def postal_code
+#   end
 
-  def credit
-  end
+#   def credit
+#   end
 
-end
+# end
