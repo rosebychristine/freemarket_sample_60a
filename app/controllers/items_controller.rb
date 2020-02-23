120183a239df4a80a318079376e6bafad3ef3197
pilons <56548247+pilons@users.noreply.github.com>
2020-02-23 11:59:50 +0900

    商品出品完了

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,12 +10,14 @@ class ItemsController < ApplicationController
     # 出品画面を表示するためのアクション
     # form_withを使うためにまずインスタンス生成をする（空っぽのインスタンスの準備）
     @product = Product.new
+    @product.images.new
+    product_condition
     end
 
     def create
     # 保存する処理
         @product = Product.create(product_params)
-        @product = Item.includes(:images).order('created_at DESC').limit(10)
+        # @product = Item.includes(:images).order('created_at DESC').limit(10)
 
     end
 
@@ -29,7 +31,11 @@ class ItemsController < ApplicationController
         @product = Product.find(params[:id])
     end
 
+    private
     def product_params
-        params.require(:product).permit(:name, :price ,:description, images_attributes: {image: []}).merge(user_id: 1)
+        params.require(:product).permit(:name, :price ,:description, :condition, images_attributes: [:src]).merge(user_id: 1)
+    end
+    def product_condition
+        @condition = ["新品、未使用","目立った傷や汚れなし","やや傷や汚れあり","傷や汚れあり","全体的に状態が悪い"]
     end
 end
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,4 @@
 class Image < ApplicationRecord
-  belongs_to :product,optional: true
   mount_uploader :src, ImageUploader
+  belongs_to :product,optional: true
 end
