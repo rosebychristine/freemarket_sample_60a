74a5429119858a47cc646be82406f5f8405365e7
rosebychristine <43508812+rosebychristine@users.noreply.github.com>
2020-03-11 16:56:01 +0900

    購入へ

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -40,12 +40,21 @@ class ItemsController < ApplicationController
     end
 
     def update
-        if @product.update(product_params)
-            redirect_to root_path
-          else 
-            redirect_to edit_item_path
-          end
+        @product = Product.find(params[:id])
+    @images = @product.images
+    card = Card.where(user_id: current_user.id).first
+    #Cardテーブルは前回記事で作成、テーブルからpayjpの顧客IDを検索
+    if card.blank?
+      #登録された情報がない場合にカード登録画面に移動
+      redirect_to controller: "card", action: "new"
+    else
+      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+      #保管した顧客IDでpayjpから情報取得
+      customer = Payjp::Customer.retrieve(card.customer_id)
+      #保管したカードIDでpayjpから情報取得、カード情報表示のためインスタンス変数に代入
+      @default_card_information = customer.cards.retrieve(card.card_id)
     end
+  end
 
     def show
         @images = @product.images
