87bd88dad480e46673e1a67d9ceb88ed00b0b4b1
rosebychristine <43508812+rosebychristine@users.noreply.github.com>
2020-03-11 16:15:18 +0900

    payjp実装

--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -1,4 +1,5 @@
 class PurchaseController < ApplicationController
+
   require 'payjp'
 
   def index
@@ -20,7 +21,7 @@ class PurchaseController < ApplicationController
     card = Card.where(user_id: current_user.id).first
     Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
     Payjp::Charge.create(
-    :amount => 13500, #支払金額を入力（itemテーブル等に紐づけても良い）
+    :amount => 10000, #支払金額を入力（itemテーブル等に紐づけても良い）
     :customer => card.customer_id, #顧客ID
     :currency => 'jpy', #日本円
   )
