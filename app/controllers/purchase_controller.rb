6e05862043f2cbe5e1ce959222228a085e1dc788
rosebychristine <43508812+rosebychristine@users.noreply.github.com>
2020-03-12 21:51:24 +0900

    値段をpayjpに反映

--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -19,12 +19,14 @@ class PurchaseController < ApplicationController
 
   def pay
     card = Card.where(user_id: current_user.id).first
+    @product = Product.find(params[:id])
     Payjp.api_key = Rails.application.credentials[:payjp][:PAYJP_SECRET_KEY]
     Payjp::Charge.create(
     amount: @product.price, #支払金額を入力（itemテーブル等に紐づけても良い）
     customer: card.customer_id, #顧客ID
     currency: 'jpy', #日本円
   )
+  
   redirect_to action: 'done' #完了画面に移動
   end
 
@@ -48,5 +50,15 @@ class PurchaseController < ApplicationController
       #保管したカードIDでpayjpから情報取得、カード情報表示のためインスタンス変数に代入
       @default_card_information = customer.cards.retrieve(card.card_id)
     end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(
+      :name,
+      :text,
+      :price,
+    ).merge(user_id: current_user.id)
   end
 end
